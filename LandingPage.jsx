82fed7b3dd46313b447f53c201f08f1e8d71c36b
Karthikeyan P <113576832+Karthi4015@users.noreply.github.com>
2024-10-21 19:53:12 +0530

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import MainSection from './Components/MainSection'
+import Footer from './Components/Footer'
+
+const LandingPage = () => {
+  return (
+   <>
+
+    <Navbar/>
+
+    <MainSection />
+
+    <Footer />
+
+
+   </>
+  )
+}
+
+export default LandingPage
